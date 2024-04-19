3dcc6399dbcd31cfb3eeb8029974a63c44672d2e
helo <annaheloisa1809@gmail.com>
2024-04-19 08:51:25 -0300

    mexi no results

---
+++ helo/results.cs
@@ -0,0 +1,20 @@
+namespace helo;
+
+public class Results
+{
+	public int Temp {get; set;}
+	public string Description {get; set;}
+    public int Date {get; set;}
+    public int Condition_cod {get; set;}
+    public int img_id {get; set;}
+	public string Currently {get; set;}
+	public string City {get; set;}
+	public int Humidity {get; set;}
+	public double Rain {get; set;}
+	public string Sunrise {get; set;}
+	public string Sunset {get; set;}
+	public string Wind_Speedy {get; set;}
+	public int Wind_Direction {get; set;}
+
+		return builder.Build();
+	}
