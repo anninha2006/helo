c15ab69851a08dc819496c59f87697f07d61cd79
helo <annaheloisa1809@gmail.com>
2024-04-23 09:01:29 -0300

    mexi no cs

--- helo/MainPage.xaml.cs
+++ helo/MainPage.xaml.cs
@@ -2,16 +2,45 @@
 
 public partial class MainPage
 {
-	const string Url = "https://api.hgbrasil.com/weather?woeid=455927&key="
+	const string Url = "https://api.hgbrasil.com/weather?woeid=455927&key=";
 	results resposta;
-	void Atualiza Tempo()
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	void TestaLayout()
+	{
+		resultado = new Results();
+		resultado.Temp = 23;
+		resultado.Description = "Tempo nublado";
+		resultado.Currently = "dia";
+		resultado.City = "Apucarana";
+
+	}
+
+	void PreencheTela()
+	{
+		labelTemperature.Text = resultado.Temp;
+		labelDescricao.Text = resultado.Description;
+	}
+	void AtualizaTempo()
 	{
 		try
 		{
 			var httpcliente= new HttpClient();
 			var response = await HttpCliente();
-			if (response.Is Success Status Code)
+			if (response.IsSuccessStatusCode)
+			{
+				String Content= response.Content.ReadAsStringAsync()
+				Resposta=JsonSerializer.Deserialize<Results>(content);
+			}
+		}
+		catch (Exception e){
+			//Erro
 		}
 	}
+
 }
 
